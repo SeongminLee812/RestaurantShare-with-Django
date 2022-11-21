a18fcaaa94349fdab6c76d37eaf52cae163a2561
seongminlee812 <sungminlee32@gmail.com>
2022-11-21 16:13:13 +0900

    category create & read function finished

--- RestaurantShare/shareRes/urls.py
+++ RestaurantShare/shareRes/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
-	path('restaurantDetail/', views.restaurantDetail),
-	path('restaurantCreate/', views.restaurantCreate),
-	path('categoryCreate/', views.categoryCreate),
+	path('restaurantDetail/', views.restaurantDetail, name='resDetailPage'),
+	path('restaurantCreate/', views.restaurantCreate, name='resCrestaPage'),
+	path('categoryCreate/', views.categoryCreate, name='cateCreatePate'),
+	path('categoryCreate/create', views.Create_category, name='cateCreate'),
 ]
--- RestaurantShare/shareRes/views.py
+++ RestaurantShare/shareRes/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import *
 
 # Create your views here.
 def index(request):
-	# return HttpResponse('index')
-	return render(request, 'shareRes/index.html')
-
+	categories = Category.objects.all()
+	content = {'categories':categories}
+	return render(request, 'shareRes/index.html', content)
+	
 def restaurantDetail(request):
 	# return HttpResponse('restaurantDetail')
 	return render(request, 'shareRes/restaurantDetail.html')
@@ -15,5 +18,13 @@ def restaurantCreate(request):
 	return render(request, 'shareRes/restaurantCreate.html')
 
 def categoryCreate(request):
-	# return HttpResponse('categoryCreate')
-	return render(request, 'shareRes/categoryCreate.html')
+	categories = Category.objects.all()
+	content = {'categories':categories}
+	return render(request, 'shareRes/categoryCreate.html', content)
+	
+def Create_category(request):
+	category_name = request.POST['categoryName']
+	new_category = Category(category_name = category_name)
+	new_category.save()
+	return HttpResponseRedirect(reverse('index'))
+	# return HttpResponse('여기서 category Create 기능 구현할 것임')
